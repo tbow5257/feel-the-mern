214a572e554029f7a352da87e6a3cc12823f9cfb
Thomas <tjbowen.52@gmail.com>
2020-04-14 13:09:22 -0700

    checkerboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,132 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import logo from './logo.svg';
+// import "bootstrap/dist/css/bootstrap.min.css";
+import axios from 'axios';
+
+const BlackSquare = () => <div className="blacksquare">B</div>;
+const WhiteSquare = () => <div className="whitesquare">W</div>;
+
+
+const makeBoard = (size) => {
+
+  const bColumn = (size) => {
+    const row = Array(size).fill().map((v, i) => {
+      if (i%2 == 0){
+        return <BlackSquare/>
+      } else {
+        return <WhiteSquare/>
+      }
+    });
+  
+    return <div>{row}</div>
+  };
+  
+  const wColumn = (size) => {
+    const row = Array(size).fill().map((v, i) => {
+      if (i%2 == 0){
+        return <WhiteSquare/>
+      } else {
+        return <BlackSquare/>
+      }
+    });
+  
+    return <div>{row}</div>
+  };
+  
+  function generate(size) {
+    const matrix = Array(size).fill().map((v, i) => {
+                      if (i%2 == 0){
+                        return bColumn(size)
+                      } else {
+                        return wColumn(size)
+                      }
+                    })
+    
+    return matrix;
+  };
+
+  return generate(size);
+}
+
+const bColumn = () => {
+  const row = Array(5).fill().map((v, i) => {
+    if (i%2 == 0){
+      return <BlackSquare/>
+    } else {
+      return <WhiteSquare/>
+    }
+  });
+
+  return <div>{row}</div>
+};
+
+const wColumn = () => {
+  const row = Array(5).fill().map((v, i) => {
+    if (i%2 == 0){
+      return <WhiteSquare/>
+    } else {
+      return <BlackSquare/>
+    }
+  });
+
+  return <div>{row}</div>
+};
+
+function generate() {
+  const matrix = Array(5).fill().map((v, i) => {
+                    if (i%2 == 0){
+                      return bColumn()
+                    } else {
+                      return wColumn()
+                    }
+                  })
+  
+  return matrix;
+};
+
+console.log(generate())
+console.log(wColumn)
 
 function App() {
+
+  // useEffect(
+  //   () => {
+  //     async function getData() {
+  //       await axios.get('http://172.18.0.3:8080/user')
+  //         .then(res => console.log('res ', res))
+  //         .catch(err => console.log('err ', err));
+  //     }
+  
+  //     getData();
+  //   },
+  //   []
+  // );
+
+  const [value, setValue] = useState(8);
+  const [size, setSize] = useState(value);
+
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit')
+    setSize(value);
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+    setValue(Number(e.target.value));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="Grid">{makeBoard(size)}</div>
+        <p>To change checkerboard, enter number and click submit </p>
+        <input type="text" name="size" onChange={handleChange}/>
+        <button onClick={handleSubmit}>Submit</button>
       </header>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 // server.js
 const express = require('express');
 const path = require("path");
-
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 8080;
 
+app.use(cors());
 // Our DB Configuration
 require('./src/database');
 
