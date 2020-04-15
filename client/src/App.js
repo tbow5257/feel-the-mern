8c18aef29409ca6d004d12cd463a145cab625e34
Thomas <tjbowen.52@gmail.com>
2020-04-14 17:10:27 -0700

    refactor board

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
-import Square from './Square'
 import CheckboardContextProvider from './CheckboardContext';
 import Colors from './Colors';
-
-const makeBoard = (size) => {
-
-  const matrix = Array(size).fill().map((v, i, t) => {
-                      if (i%2 == 0){
-                        return evenRow(size, i)
-                      } else {
-                        return oddRow(size, i)
-                      }
-                    })
-
-  return matrix;
-
-  function shapeRows(){
-    const collection = new Set();
-
-    collection.add(0,1);
-    collection.add(size-1);
-    collection.add(size);
-  }
-
-  function evenRow(size, rowNum) {
-    const row = Array(size).fill().map((v, i, t) => {
-      const size = t.length;
-      if (i%2 == 0){
-        return <Square btype="blacksquare" row={rowNum} totalSize={size}/>
-      } else {
-        return <Square btype="whitesquare" row={rowNum} totalSize={size}/>
-      }
-    });
-  
-    return <div className="Checker-Row">{row}</div>
-  };
-  
-  function oddRow(size, rowNum) {
-    const row = Array(size).fill().map((v, i, t) => {
-      const size = t.length;
-      if (i%2 == 0){
-        return <Square btype="whitesquare" row={rowNum} totalSize={size}/>
-      } else {
-        return <Square btype="blacksquare" row={rowNum} totalSize={size}/>
-      }
-    });
-  
-    return <div className="Checker-Row">{row}</div>
-  };
-  
-};
+import makeBoard from './Board';
 
 
 function App() {
 
-  const [colors, setColors] = useState("default");
   const [value, setValue] = useState(8);
   const [size, setSize] = useState(value);
 
---
+++ client/src/Board.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Square from './Square'
+
+const makeBoard = (size) => {
+
+    const matrix = Array(size).fill().map((v, i, t) => {
+                        if (i%2 == 0){
+                          return evenRow(size, i)
+                        } else {
+                          return oddRow(size, i)
+                        }
+                      })
+  
+    return matrix;
+  
+    function shapeRows(){
+      const collection = new Set();
+  
+      collection.add(0,1);
+      collection.add(size-1);
+      collection.add(size);
+    }
+  
+    function evenRow(size, rowNum) {
+      const row = Array(size).fill().map((v, i, t) => {
+        const size = t.length;
+        if (i%2 == 0){
+          return <Square btype="blacksquare" row={rowNum} totalSize={size}/>
+        } else {
+          return <Square btype="whitesquare" row={rowNum} totalSize={size}/>
+        }
+      });
+    
+      return <div className="Checker-Row">{row}</div>
+    };
+    
+    function oddRow(size, rowNum) {
+      const row = Array(size).fill().map((v, i, t) => {
+        const size = t.length;
+        if (i%2 == 0){
+          return <Square btype="whitesquare" row={rowNum} totalSize={size}/>
+        } else {
+          return <Square btype="blacksquare" row={rowNum} totalSize={size}/>
+        }
+      });
+    
+      return <div className="Checker-Row">{row}</div>
+    };
+    
+  };
+
+  export default makeBoard;
